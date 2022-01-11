704a8cacb9498977254f8f39f402de36ba09e47e
bugattie <m.mohsinbarkaati@gmail.com>
2022-01-11 18:46:22 +0500

    user login embedded with frontend

--- frontend/src/rootReducer.js
+++ frontend/src/rootReducer.js
@@ -5,11 +5,13 @@ import {
   productDetailReducer,
 } from "./reducers/productReducers.js";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const rootReducer = combineReducers({
   productList: productListReducer,
   productDetail: productDetailReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 export default rootReducer;
